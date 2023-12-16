acbf26f30be28e8696eec8c37515135cbd85ed1d
Filip Kaniecki <f.kaniecki@outlook.com>
2023-12-16 01:30:27 +0100

    Initial commit

---
+++ src/test/java/pl/studia/przetwarzanie/rozproszone/PrzetwarzanieRozproszoneApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.studia.przetwarzanie.rozproszone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrzetwarzanieRozproszoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
